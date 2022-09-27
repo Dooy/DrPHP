5db5b33cfdff59d56b6aa09f101e4bbf37a2c675
Dooy <dooy520@qq.com>
2022-09-27 20:57:12 +0800

    执行docker 里面的 php

---
+++ dockerPhp.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+#file = grep  $1
+iput=$( echo "$1" | grep -o "[^\.\/]\+\(\+[^ ]\+\)*" )
+iput=$1
+vfile="$(pwd)/"$iput
+ydr="\/Users\/wulin\/Documents\/yangdaorong\/"
+dk="\/data\/webRoot\/"
+
+news=$(echo $vfile | sed "s/$ydr/$dk/g")
+
+echo "File is : docker exec php5.6fpm php -f ${news}"
+
+docker exec php5.6fpm php -f $news
+
+
